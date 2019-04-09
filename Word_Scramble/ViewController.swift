ed002a3dfb1dbd3107b8951b991a36a3d9c05022
Jassem Al-Buloushi <jassem1988@gmail.com>
2019-04-09 12:49:02 +0300

    Initial Commit

---
+++ Word_Scramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word_Scramble
+//
+//  Created by Jassem Al-Buloushi on 4/9/19.
+//  Copyright © 2019 Jassem Al-Buloushi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
